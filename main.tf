4c578b8029c355b8d1ee00c4865b8a3d4ee7dd94
Shivanirshankar <shivaniravishankar78@gmail.com>
2025-01-31 11:43:50 +0530

    AMI

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "web" {
   ami           = "ami-04f77c9cd94746b09"  # Microsoft Windows Server 2025
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "GitHub-CI-CD-VM"
